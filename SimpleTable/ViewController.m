dde56f312711120a83918037f790eedf6b5e78da
Elvis Lee <elvis.lee@movial.com>
2014-07-22 14:16:44 +0800

    Add image for each cell.

--- SimpleTable/ViewController.m
+++ SimpleTable/ViewController.m
@@ -14,12 +14,15 @@
 
 @implementation ViewController
 NSArray *tableData;
+NSArray *thumbnails;
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
     tableData = [NSArray arrayWithObjects:@"Egg Benedict", @"Mushroom Risotto", @"Full Breakfast", @"Hamburger", @"Ham and Egg Sandwich", @"Creme Brelee", @"White Chocolate Donut", @"Starbucks Coffee", @"Vegetable Curry", @"Instant Noodle with Egg", @"Noodle with BBQ Pork", @"Japanese Noodle with Pork", @"Green Tea", @"Thai Shrimp Cake", @"Angry Birds Cake", @"Ham and Cheese Panini", nil];
+
+    thumbnails = [NSArray arrayWithObjects:@"egg_benedict.jpg", @"mushroom_risotto.jpg", @"full_breakfast.jpg", @"hamburger.jpg", @"ham_and_egg_sandwich.jpg", @"creme_brelee.jpg", @"white_chocolate_donut.jpg", @"starbucks_coffee.jpg", @"vegetable_curry.jpg", @"instant_noodle_with_egg.jpg", @"noodle_with_bbq_pork.jpg", @"japanese_noodle_with_pork.jpg", @"green_tea.jpg", @"thai_shrimp_cake.jpg", @"angry_birds_cake.jpg", @"ham_and_cheese_panini.jpg", nil];
 }
 
 - (void)didReceiveMemoryWarning
@@ -44,7 +47,7 @@ NSArray *tableData;
     }
     
     cell.textLabel.text = [tableData objectAtIndex:indexPath.row];
-    cell.imageView.image = [UIImage imageNamed:@"creme_brelee.jpg"];
+    cell.imageView.image = [UIImage imageNamed:[thumbnails objectAtIndex:indexPath.row]];
     return cell;
 }
 @end
